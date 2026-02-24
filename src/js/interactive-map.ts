05694c81011397e3f89e581985ab7a5fe6254ed1
ouzepe <je.ouzepe@gmail.com>
2026-02-24 07:01:39 +0100

    fix button disabled

--- src/js/interactive-map.ts
+++ src/js/interactive-map.ts
@@ -70,6 +70,14 @@ document.addEventListener("DOMContentLoaded", () => {
     document.documentElement.classList.remove("im-drawer-open");
   };
 
+  const buttons = document.querySelectorAll(".wp-block-button__link");
+
+  buttons.forEach((btn) => {
+    // Si le lien n'a pas d'attribut href ou est vide
+    if (!btn.getAttribute("href") || btn.getAttribute("href")?.trim() === "") {
+      btn.classList.add("disabled-button");
+    }
+  });
   const openDrawer = () => {
     if (!drawer) return;
     drawer.classList.add("open");
